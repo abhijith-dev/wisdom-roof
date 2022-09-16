b2fff822c00d4b0fc10cb92c06516868e6a31db7
Abhijithdev123 <abhjith.s.partner@decathlon.com>
2022-09-16 23:44:28 +0530

    v1.0.0 init-commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const helmet = require('helmet')
+
+const app =  express()
+
+//security
+app.use(helmet.contentSecurityPolicy())
+app.use(helmet.crossOriginEmbedderPolicy())
+app.use(helmet.crossOriginOpenerPolicy())
+app.use(helmet.crossOriginResourcePolicy())
+app.use(helmet.dnsPrefetchControl())
+app.use(helmet.expectCt())
+app.use(helmet.frameguard())
+app.use(helmet.hidePoweredBy())
+app.use(helmet.hsts())
+app.use(helmet.ieNoOpen())
+app.use(helmet.noSniff())
+app.use(helmet.originAgentCluster())
+app.use(helmet.permittedCrossDomainPolicies())
+app.use(helmet.referrerPolicy())
+app.use(helmet.xssFilter())
+app.disable('x-powered-by')
+
+//default middlewares
+
+app.use(express.json())
+
+//routes
+
+//
+
+
